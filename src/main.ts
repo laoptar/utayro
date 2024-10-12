79c6dc3d37726f3da11babb0a862a5db561f36e8
王卫国 <wbwangwg@szlanyou.com>
2024-10-12 11:44:21 +0800

    px2vw

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+// reset css
+import "normalize.css/normalize.css";
+
+// vue init
+const app = createApp(App);
+app.use(store);
+app.use(router);
+app.mount("#app");
